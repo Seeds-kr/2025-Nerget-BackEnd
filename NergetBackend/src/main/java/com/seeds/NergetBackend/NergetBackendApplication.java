efd86f61f5a414833dbcc2f62e07397e55d42ae5
junseokPP <pjs981130@gmail.com>
2025-04-09 19:11:18 +0900

    start

---
+++ NergetBackend/src/main/java/com/seeds/NergetBackend/NergetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seeds.NergetBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NergetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NergetBackendApplication.class, args);
+	}
+
+}
