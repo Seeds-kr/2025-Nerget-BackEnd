bcde2d51d1d43b5a7d71353388135cce736ee025
junseokPP <pjs981130@gmail.com>
2025-08-04 23:36:55 +0900

    Real Main 모든 클래스 삭제완료 구글 배포완료

--- NergetBackend/src/main/java/com/seeds/NergetBackend/repository/SwipeFeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seeds.NergetBackend.repository;
-
-import com.seeds.NergetBackend.entity.SwipeFeedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SwipeFeedbackRepository extends JpaRepository<SwipeFeedback, Long> {
-}
--- NergetBackend/src/main/java/com/seeds/NergetBackend/service/SwipeService.java
+++
@@ -1,7 +0,0 @@
-package com.seeds.NergetBackend.service;
-
-import com.seeds.NergetBackend.dto.SwipeRequestDto;
-
-public interface SwipeService {
-    void saveFeedback(SwipeRequestDto dto);
-}
