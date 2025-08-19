c94f85efe9cd3b5d208677614cf0df441a68b793
Taekyun Lee <william4@naver.com>
2025-08-19 20:27:09 +0900

    수정

--- src/main/java/com/seeds/NergetBackend/controller/MyPageController.java
+++ src/main/java/com/seeds/NergetBackend/controller/MyPageController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/mypage")
-public class MyPageController {
+public class MyPag eController {
 
     private final MyPageService myPageService;
 
--- src/main/java/com/seeds/NergetBackend/repository/CommentRepository.java
+++ src/main/java/com/seeds/NergetBackend/repository/CommentRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByPostOrderByCreatedAtAsc(Post post); // 게시글 기준 댓글 목록
 
-    // 내 댓글 첫 페이지
+
     List<Comment> findByAuthorEmailOrderByIdDesc(String email, org.springframework.data.domain.Pageable pageable);
 
-    // 내 댓글 다음 페이지
+
     List<Comment> findByAuthorEmailAndIdLessThanOrderByIdDesc(String email, Long cursor, org.springframework.data.domain.Pageable pageable);
 
 }
